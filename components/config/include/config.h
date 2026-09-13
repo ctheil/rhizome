aaaddc77ae3d291d946e9eb8f1eaa7b6192534c3
Caleb Theil <86980706+ctheil@users.noreply.github.com>
2026-09-13 10:44:54 -0600

    adds: better def for config

--- components/config/include/config.h
+++ components/config/include/config.h
@@ -6,8 +6,24 @@
 
 typedef struct
 {
-  uint8_t enable_reservoir_sensor;
-  uint16_t max_reservoir_depth_cm;
+  bool enabled;
+  uint8_t echo_pin;
+  uint8_t trigger_pin;
+  uint16_t reservoir_depth_cm;
+} reservoir_config_t;
+
+typedef struct
+{
+  bool enabled;
+  uint8_t in1_pin;
+  uint8_t in2_pin;
+  uint64_t max_run_time_ms;
+} pump_config_t;
+
+typedef struct
+{
+  reservoir_config_t reservoir;
+  pump_config_t pumps[2];
 } config_t;
 
 #endif /* __CONFIG_H__ */
